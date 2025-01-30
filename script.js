e2519c1ba0d8e748f3c8f558d65807b990e2806d
joaoqys <135122635+joaoqys@users.noreply.github.com>
2025-01-30 03:17:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const securityIndicatorBarEl = document.querySelector(
   "#security-indicator-bar"
 )
 
-let passwordLength = 3
+let passwordLength = 16
 
 function generatePassword() {
   let chars = "abcdefghjkmnpqrstuvwxyz"
